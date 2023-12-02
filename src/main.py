df37350ef18672f54c24614ae8ef976e4d0f4a64
Ryusuke-I <ryusuke.iizuka@oneroots.net>
2023-12-02 17:44:24 +0900

    コミット

--- src/main.py
+++ src/main.py
@@ -7,10 +7,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-# 環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = "H8uf7m0QRWdAQ7SnjYruob/MPQfDtMmp5K4tYudxMK2E5K12zg4/DFIbhI9C+jCniXDsP20v9TmwsHLTTP9rBHCj54X2u/xVSPG2X7CwBly6VcwntYTQZSDb138onzlu7XbuWYLzRA6Zbie9q6P5pAdB04t89/1O/w1cDnyilFU="
 YOUR_CHANNEL_SECRET = "69928433508679b4dc564a8834cb25d2"
-
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -42,6 +40,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
